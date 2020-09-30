231ac8d3704733c69b18ce8cb62dc4fc5a3406c4
yiyangii <yiyang6004@gmail.com>
2020-09-29 22:50:10 -0400

    Second commit

--- app/src/main/java/edu/temple/coloractivity/ColorAdapter.java
+++ app/src/main/java/edu/temple/coloractivity/ColorAdapter.java
@@ -1,56 +1,73 @@
 package edu.temple.coloractivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
-import android.graphics.Color;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class ColorAdapter extends BaseAdapter {
+
     Context context;
     ArrayList<String> items;
     int colors[];
-    public ColorAdapter(Context context,ArrayList<String> items){
+
+    public ColorAdapter(Context context, ArrayList<String> items){
         this.context = context;
         this.items = items;
-        colors = new int[6];
+
+        colors = new int[11];
         colors[0] = Color.WHITE;
-        colors[1] = Color.RED;
-        colors[2] = Color.YELLOW;
-        colors[3] = Color.GREEN;
-        colors[4] = Color.LTGRAY;
-        colors[5] = Color.BLUE;
-        colors[6] = Color.GRAY;
+        colors[1] = Color.BLACK;
+        colors[2] = Color.BLUE;
+        colors[3] = Color.CYAN;
+        colors[4] = Color.DKGRAY;
+        colors[5] = Color.GRAY;
+        colors[6] = Color.GREEN;
+        colors[7] = Color.LTGRAY;
+        colors[8] = Color.MAGENTA;
+        colors[9] = Color.RED;
+        colors[10] = Color.TRANSPARENT;
     }
+
+    @Override
     public int getCount() {
         return items.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return items.get(i);
+    public Object getItem(int position) {
+        return items.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
+    public long getItemId(int position) {
         return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         TextView textView;
-        if(convertView == null){
+        if (convertView == null) {
             textView = new TextView(context);
-            textView.setTextSize(5);
+            textView.setTextSize(20);
             textView.setPadding(5,5,5,5);
-        }else{
-            textView = (TextView)convertView;
-            textView.setText(getItem(position).toString());
-            textView.setBackgroundColor(colors [position% colors.length]);
         }
-        return null;
+        else
+            textView = (TextView) convertView;
+
+        textView.setText(getItem(position).toString());
+        textView.setBackgroundColor(colors[position % colors.length]);
+        return textView;
     }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        View v = getView(position, convertView, parent);
+        return v;
+    }
+
 }
--- app/src/main/java/edu/temple/coloractivity/MainActivity.java
+++ app/src/main/java/edu/temple/coloractivity/MainActivity.java
@@ -1,7 +1,6 @@
 package edu.temple.coloractivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -11,47 +10,71 @@ import android.widget.Spinner;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    // inital layout spinner layour view
     Spinner spinner;
     View layout;
+
+    ArrayList<String> items;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> items = new ArrayList<String>();
-        items.add("Color:");
-
-        items.add("RED");
-        items.add("YELLOW");
-        items.add("GREEN");
-        items.add("LIGHTGREY");
-        items.add("BLUE");
-        items.add("GRAY");
-
         spinner = findViewById(R.id.spinner);
-        layout = findViewById(R.id.layout);
+        layout = findViewById(R.id.layoutview);
 
-        final BaseAdapter adapter = new ColorAdapter(this,items);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        items = new ArrayList<>();
+
+        items.add("Selecte Color");
+        items.add("BLACK");
+        items.add("BLUE");
+        items.add("CYAN");
+        items.add("DKGRAY");
+        items.add("GRAY");
+        items.add("GREEN");
+        items.add("LTGRAY");
+        items.add("MAGENTA");
+        items.add("RED");
+        items.add("TRANSPARENT");
+
+
+
+        final BaseAdapter coloradapter = new ColorAdapter(this, items);
+        spinner.setAdapter(coloradapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 view.setBackgroundColor(Color.WHITE);
-                if(spinner.getSelectedItem().toString().equals(("RED"))){
-                    layout.setBackgroundColor(Color.RED);
-                }else if(spinner.getSelectedItem().toString().equals(("YELLOW"))){
-                    layout.setBackgroundColor(Color.YELLOW);
-                }else if(spinner.getSelectedItem().toString().equals(("GREEN"))){
-                    layout.setBackgroundColor(Color.GREEN);
-                }else if(spinner.getSelectedItem().toString().equals(("LIGHTGREY"))){
-                    layout.setBackgroundColor(Color.LTGRAY);
-                }else if(spinner.getSelectedItem().toString().equals(("BLUE"))){
+                if (spinner.getSelectedItem().toString().equals("BLACK")) {
+                    layout.setBackgroundColor(Color.BLACK);
+                } else if (spinner.getSelectedItem().toString().equals("BLUE")) {
                     layout.setBackgroundColor(Color.BLUE);
-                }else if(spinner.getSelectedItem().toString().equals(("GRAY"))){
+                } else if (spinner.getSelectedItem().toString().equals("CYAN")) {
+                    layout.setBackgroundColor(Color.CYAN);
+                } else if (spinner.getSelectedItem().toString().equals("DKGRAY")) {
+                    layout.setBackgroundColor(Color.DKGRAY);
+                } else if (spinner.getSelectedItem().toString().equals("GRAY")) {
                     layout.setBackgroundColor(Color.GRAY);
+                }else if (spinner.getSelectedItem().toString().equals("GREEN")) {
+                    layout.setBackgroundColor(Color.GREEN);
+                }else if (spinner.getSelectedItem().toString().equals("LTGRAY")) {
+                    layout.setBackgroundColor(Color.LTGRAY);
+                }else if (spinner.getSelectedItem().toString().equals("MAGENTA")) {
+                    layout.setBackgroundColor(Color.MAGENTA);
+                }else if (spinner.getSelectedItem().toString().equals("RED")) {
+                    layout.setBackgroundColor(Color.RED);
+                }else if (spinner.getSelectedItem().toString().equals("TRANSPARENT")) {
+                    layout.setBackgroundColor(Color.TRANSPARENT);
                 }
             }
-        });
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
